45b764c95e953279fa19b555f89b6bf72b6fbe50
mnkstar <103582688+mnkstar@users.noreply.github.com>
2023-11-14 14:27:40 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose
@@ -14,9 +15,14 @@ mongoose
 });
 
 const app = express();
+
+app.use(express.json());
+
 app.listen(8080,()=>{
     console.log('server started at port 8080');
 }
 );
 
 app.use('/api/user',userRouter);
+app.use('/api/auth', authRouter);
+
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -10,11 +10,14 @@ const userSchema = new mongoose.Schema({
     email:{
         type:String,
         required:true,
-    }
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+    }, 
 },
-    {
-        timestamp:true,
-    }
+    {timestamps:true,}
 );
 
 const User = mongoose.model('User', userSchema);
