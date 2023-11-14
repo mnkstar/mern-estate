4a2f63a571e4bf22edaa4c97c6a66d5ffce0516d
mnkstar <103582688+mnkstar@users.noreply.github.com>
2023-11-14 12:23:47 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const app = express()
-app.listen(8080, ()=>{
-    console.log("Server is running in port 8080");
+mongoose
+.connect(process.env.MONGO)
+.then(() => {
+    console.log("CONNECTION TO MONGODB SUCCESSFULL!!");
+})
+.catch((err) => {
+    console.log(err);
+});
+
+const app = express();
+app.listen(8080,()=>{
+    console.log('server started at port 8080');
 }
 );
