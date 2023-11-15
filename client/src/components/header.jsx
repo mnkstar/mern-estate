c981434113069ea9aacae81bb6defabd615e7ecc
mnkstar <103582688+mnkstar@users.noreply.github.com>
2023-11-15 14:23:19 +0530

    add redux persist

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -30,6 +30,7 @@ export default function Header() {
             <Link to='/'><li className='hidden sm:inline text-slate-700 hover:underline'>Home</li></Link>
             <Link to='/about'> <li className='hidden sm:inline text-slate-700 hover:underline'>About</li></Link>
             <Link to='sign-in'>  <li className='text-slate-700 hover:underline'>Sign-in</li></Link>
+            <Link to='sign-up'>  <li className='text-slate-700 hover:underline'>Sign-up</li></Link>
         </ul>
       </div>
     </header>
