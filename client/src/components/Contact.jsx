5ffa649f810d1cf238478961b41230f20e94489f
mnkstar <103582688+mnkstar@users.noreply.github.com>
2023-11-21 17:13:07 +0530

    create search api route

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -2,13 +2,13 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-function Contact({listing}) {
+function Contact({ listing }) {
   const [landlord, setLandlord] = useState(null);
-  const [message, setMessage] = useState(' ');
+  const [message, setMessage] = useState(" ");
 
-  const onChange = (e)=>{
-    setMessage(e.target.value)
-  }
+  const onChange = (e) => {
+    setMessage(e.target.value);
+  };
 
   useEffect(() => {
     const fetchLandlord = async () => {
@@ -19,33 +19,46 @@ function Contact({listing}) {
       } catch (error) {
         console.log(error);
       }
-    }
+    };
     fetchLandlord();
   }, [listing.userRef]);
 
-  return <>
-  {landlord && (
-    <div className="flex flex-col gap-3">
-        <p>
-            Contact <span className="font-semibold">{landlord.username}</span> at   
-            <span className="text-red-700 font-bold hover:underline"> {landlord.email}</span> for <span className="font-semibold">
-                {listing.name.toLowerCase()}
-            </span>
-        </p>
-        <textarea name="message" id="message" rows="2" 
-        placeholder="Your message" 
-        value={message} 
-        onChange={onChange}
-        className="w-full border p-3 rounded-lg">
-     </textarea>
-     <Link className="bg-red-700 text-white font-semibold text-center p-3 rounded-lg uppercase hover:bg-green-700"
-     to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}>
-      Send your message
-     </Link>
-
-    </div>
-  )}
-  </>
+  return (
+    <>
+      {landlord && (
+        <div className="flex flex-col gap-3">
+          <p>
+            Contact <span className="font-semibold">{landlord.username}</span>{" "}
+            at
+            <Link
+              className="text-red-700 font-bold hover:underline"
+              to={`mailto:${landlord.email}?subject=Regarding ${listing.name}`}
+            >
+              {" "}
+              {landlord.email}
+            </Link>{" "}
+            for{" "}
+            <span className="font-semibold">{listing.name.toLowerCase()}</span>
+          </p>
+          <textarea
+            name="message"
+            id="message"
+            rows="2"
+            placeholder="Your message"
+            value={message}
+            onChange={onChange}
+            className="w-full border p-3 rounded-lg"
+          ></textarea>
+          <Link
+            className="bg-red-700 text-white font-semibold text-center p-3 rounded-lg uppercase hover:bg-green-700"
+            to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
+          >
+            Send your message
+          </Link>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Contact;
