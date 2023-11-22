be71b894339b279b22000142cc7238bca24837a8
mnkstar <103582688+mnkstar@users.noreply.github.com>
2023-11-22 17:32:01 +0530

    complete deploy page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,36 @@
-
-
 function About() {
   return (
-    <div>
-      About
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl text-slate-700 font-bold mb-4">About <span className="text-slate-500">Get
+        </span>
+        <span>Estate</span></h1>
+      <p className="mb-4 text-slate-700 font-bold">
+        Welcome to GetEstate Realty, where your property dreams come to life!
+        🏡✨ At GetEstate Realty, we specialize in bringing you the finest real
+        estate opportunities in the most sought-after locations. Whether you are
+        looking to buy, sell, or rent, our team of expert agents is dedicated to
+        making your real estate journey seamless and enjoyable. 
+        </p> <p className="mb-4 text-slate-700 font-bold">
+        Explore our extensive portfolio of properties, ranging from cozy apartments with
+        breathtaking views to luxurious estates that redefine opulence. Our
+        listings showcase a variety of styles and features, ensuring theres
+        something for every taste and preference. What sets GetEstate Realty
+        apart is our commitment to delivering great deals. We understand that
+        real estate is a significant investment, and we take pride in
+        negotiating the best possible terms for our clients. 
+        </p>
+       
+        <p className="mb-4 text-slate-700 font-bold">
+        Our experienced professionals work tirelessly to ensure that you not only find the
+        perfect property but also secure it at a price that fits your budget.
+        Whether you are a first-time buyer, a seasoned investor, or someone
+        looking to lease a property, GetEstate Realty is your trusted partner in
+        the world of real estate. Let us turn your property aspirations into a
+        reality – because at GetEstate Realty, your dream home is just a click
+        away! 🌟🔑
+      </p>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
